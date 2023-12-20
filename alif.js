5566b047ad91faf765deca4ebc66b9e6d85d4148
alifnurcahyo <alifnurcahyo1306.com>
2023-12-20 11:59:08 +0700

    upload

---
+++ alif.js
@@ -0,0 +1,15 @@
+// alif.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle button click
+    function handleButtonClick() {
+        alert("Button clicked!"); // Gantilah ini dengan aksi yang sesuai
+    }
+
+    // Get the download button element
+    var downloadButton = document.querySelector('.tombol-download');
+    var downloadButton = document.querySelector('.grup-tombol');
+    var downloadButton = document.querySelector('.navigation') ;
+    // Add a click event listener to the button
+    downloadButton.addEventListener('click', handleButtonClick);
+});
